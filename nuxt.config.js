b5ee620098f2c028a656265d37147f8ef33645bc
Jake Dohm <jake@steadfastdesignfirm.com>
2020-09-09 23:47:11 -0400

    Updated website title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: 'Jake Dohm - Tailwind UI Application',
+    title: 'Jake Dohm',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
